484ef2677766188cd8cad96c641b3f0f63b89563
Jo_Kastro <joaquimcastro2909@gmail.com>
2024-01-09 14:01:21 -0300

    Added switch_life_state function

--- lib/game_of_life/grid.ex
+++ lib/game_of_life/grid.ex
@@ -14,6 +14,19 @@ defmodule GameOfLife.Grid do
     }
   end
 
+  def switch_life_state(grid, x, y) do
+    new_cell_state = grid.cells
+      |> elem(x)
+      |> !elem(y)
+
+    new_row = grid.cells
+      |> elem(x)
+      |> put_elem(y, new_cell_state)
+
+    upd_cells = put_elem(grid.cells, x, new_row)
+    %__MODULE__{grid | cells: upd_cells}
+  end
+
   defp init_cells(size) do
     List.duplicate(false, size)
       |> List.to_tuple()
