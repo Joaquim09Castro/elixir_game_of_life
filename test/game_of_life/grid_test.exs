992ac0be847610f8d7e3e1460d58055b3b89c5a4
Jo_Kastro <joaquimcastro2909@gmail.com>
2024-01-09 13:32:29 -0300

    Grid testing done

--- test/game_of_life/grid_test.exs
+++ test/game_of_life/grid_test.exs
@@ -9,4 +9,17 @@ defmodule GameOfLife.GridTest do
 
     assert grid.size == size
   end
+
+  test "new/1 Initiates a multidimentional array of specified size" do
+    size = 3
+    grid = Grid.new(size)
+
+    expected_grid = {
+      {false, false, false},
+      {false, false, false},
+      {false, false, false}
+    }
+
+    assert expected_grid == grid.cells
+  end
 end
