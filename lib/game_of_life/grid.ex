e49e6827878bf1229ad0cb4919b3ac36f91bac30
Jo_Kastro <joaquimcastro2909@gmail.com>
2024-01-10 20:31:54 -0300

    Implementation of main game logic (state swich conditions)

--- lib/game_of_life/grid.ex
+++ lib/game_of_life/grid.ex
@@ -14,6 +14,13 @@ defmodule GameOfLife.Grid do
     }
   end
 
+  defp init_cells(size) do
+    List.duplicate(false, size)
+      |> List.to_tuple()
+      |> List.duplicate(size)
+      |> List.to_tuple()
+  end
+
   def switch_life_state(grid, x, y) do
     new_cell_state =
       !( grid.cells
@@ -28,11 +35,45 @@ defmodule GameOfLife.Grid do
     %__MODULE__{grid | cells: upd_cells}
   end
 
-  defp init_cells(size) do
-    List.duplicate(false, size)
-      |> List.to_tuple()
-      |> List.duplicate(size)
-      |> List.to_tuple()
+  defp is_active?(grid, x, y) do
+    grid.cells
+      |> elem(x)
+      |> elem(y)
   end
 
+  defp should_be_active?(grid, x, y) do
+    neighbours = get_actives(grid, x, y)
+    is_active? = is_active?(grid, x, y)
+
+    cond do
+      is_active? && neighbours < 2 ->
+        false
+      is_active? && neighbours > 3 ->
+        false
+      !(is_active?) && neighbours == 3 ->
+        true
+      is_active? && neighbours in 2..3 ->
+        true
+    end
+
+  end
+
+  defp get_neighbours(grid, x, y) do
+    for x_area <- (x - 1..x + 1),
+        y_area <- (y - 1..y + 1),
+        x_area >= 0,
+        y_area >= 0,
+        x_area < grid.size,
+        y_area < grid.size,
+        x_area != x or y_area != y do
+      {x_area, y_area}
+    end
+  end
+
+  defp get_actives(grid, x, y) do
+    grid
+      |> get_neighbours(x, y)
+      |> Enum.map(fn {x_area, y_area} -> is_active?(grid, x_area, y_area) end)
+      |> Enum.count(& &1 == true)
+  end
 end
